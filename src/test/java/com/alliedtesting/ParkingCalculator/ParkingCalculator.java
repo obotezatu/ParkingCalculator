78e29025663a0c331c8ae7c010cadfc847d38b7e
Oleg Botezatu <obotezatu@gmail.com>
2019-05-21 00:03:21 +0300

    commit 3

--- src/test/java/com/alliedtesting/ParkingCalculator/ParkingCalculator.java
+++ src/test/java/com/alliedtesting/ParkingCalculator/ParkingCalculator.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -14,21 +15,27 @@ import java.util.concurrent.TimeUnit;
 
 public class ParkingCalculator {
 
-    @Test
-    public void test() throws InterruptedException {
+	@DataProvider(name = "Times", parallel = true)
+	public Object[][] getData(){
+		return new Object[][] {
+			{"3:00","PM","3/18/2019", "7:00","PM","3/20/2019", "$ 72.00", "(2 Days, 4 Hours, 0 Minutes)"},
+			{"3:00","AM","3/20/2019", "10:00","AM","3/25/2019", "$ 180.00", "(5 Days, 7 Hours, 0 Minutes)"},
+			{"1:00","PM","5/5/2019", "15:30","PM","5/5/2019", "$ 30.00", "(0 Days, 14 Hours, 30 Minutes)"},
+			{"15:00","PM","3/15/2019", "7:00","AM","3/16/2019", "$ 12.00", "(0 Days, 4 Hours, 0 Minutes)"},
+			{"00:00","AM","3/1/2019", "00:00","AM","3/30/2019", "$ 882.00", "(29 Days, 0 Hours, 0 Minutes)"}
+		};
+	}
+	
+    @Test(dataProvider = "Times")
+    public void test(String sTime,String sradioVal, String sDate,String eTime,String eradioVal, String eDate, String cost, String calcTime) throws InterruptedException {
     	System.setProperty("webdriver.chrome.driver","/media/sef/Archive/ADMINISTRATOR/JAVA/USM_2018/Automation 2019/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-        driver.get("https://bnm.md/en/official_exchange_rates?get_xml=1&date=13.02.2018");
         driver.get("http://adam.goucher.ca/parkcalc/index.php");
-     
-
+          
         Assert.assertEquals(driver.getTitle(), "Parking Calculator");
-
-//        WebElement chooseALot = driver.findElement(By.xpath("//tr[td[contains(text(),'Choose a Lot')]]//select[@id='Lot']"));
-//        Select chooseALotSelect = new Select(chooseALot);
-
+        
         Select chooseALotSelect = new Select(driver.findElement(By.xpath("//tr[td[contains(text(),'Choose a Lot')]]//select[@id='Lot']")));
         chooseALotSelect.selectByVisibleText("Valet Parking");
 
@@ -37,35 +44,32 @@ public class ParkingCalculator {
         WebElement startDate = driver.findElement(By.xpath("//tr[td[contains(text(),'Choose Entry Date and Time')]]//input[@name='EntryDate']"));
 
         startTime.clear();
-        startTime.sendKeys("3:00");
-        selectRadioValue(startDateAmPm, "PM");
+        startTime.sendKeys(sTime);
+        selectRadioValue(startDateAmPm, sradioVal);
         startDate.clear();
-        startDate.sendKeys("3/18/2019");
-
+        startDate.sendKeys(sDate);
 
         WebElement endTime = driver.findElement(By.xpath("//tr[td[contains(text(),'Choose Leaving Date and Time')]]//input[@name='ExitTime']"));
         List<WebElement> endDateAmPm = driver.findElements(By.xpath("//tr[td[contains(text(),'Choose Leaving Date and Time')]]//input[@name='ExitTimeAMPM']"));
         WebElement endDate = driver.findElement(By.xpath("//tr[td[contains(text(),'Choose Leaving Date and Time')]]//input[@name='ExitDate']"));
 
-
         endTime.clear();
-        endTime.sendKeys("7:00");
-        selectRadioValue(endDateAmPm, "PM");
+        endTime.sendKeys(eTime);
+        selectRadioValue(endDateAmPm, eradioVal);
         endDate.clear();
-        endDate.sendKeys("3/20/2019");
-
+        endDate.sendKeys(eDate);
 
         WebElement submitBtn = driver.findElement(By.xpath("//input[@name='Submit' and @value='Calculate']"));
-
         submitBtn.click();
-
         Thread.sleep(1000);
 
         WebElement costValue = driver.findElement(By.xpath("//tr[contains(.,'COST')]/td//b[contains(text(),'$')]"));
         WebElement calculatedTime = driver.findElement(By.xpath("//tr[contains(.,'COST')]/td//b[contains(text(),'Days')]"));
-
-        Assert.assertEquals(costValue.getText(),"$ 72.00");
-        Assert.assertEquals(calculatedTime.getText().trim(), "(2 Days, 4 Hours, 0 Minutes)");
+        
+        Assert.assertEquals(costValue.getText(),cost);
+        Assert.assertEquals(calculatedTime.getText().trim(), calcTime);
+        
+        driver.quit();
     }
 
     public void selectRadioValue(List<WebElement> list, String selectValue){
